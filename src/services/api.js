a72f56fe022fca3f033d3d00c015804fcad049cf
PeerJack22 <pajocina.11@gmail.com>
2026-02-26 10:14:16 -0500

    Para iniciar sesión con el api

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 // Para la url del api con el .env
 const api = axios.create({
-baseURL: import.meta.env.VITE_API_URL || 'http://localhost:4000'
+baseURL: import.meta.env.API_URL
 })
 
 export default api
