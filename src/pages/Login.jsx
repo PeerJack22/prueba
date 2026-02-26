aeb6fd76c1f80a5b9b8dd69720af8fb186c31cdd
PeerJack22 <pajocina.11@gmail.com>
2026-02-26 10:30:58 -0500

    Para iniciar sesión con el api

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -17,7 +17,7 @@ function Login() {
             setLoading(true)
             setError('')
             
-            const response = await api.post('api/auth/login', {
+            const response = await api.post('/auth/login', {
             email,
             clave
             })
--- src/services/api.js
+++ src/services/api.js
@@ -1,8 +1,7 @@
 import axios from 'axios'
 
-// Para la url del api con el .env
 const api = axios.create({
-baseURL: import.meta.env.API_URL
+baseURL: import.meta.env.VITE_API_URL || 'http://localhost:4000'
 })
 
 export default api
